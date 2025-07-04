dba17d361ea04e482f7aacc821ac159c041f4e1d
PREET157 <balpreetkaur0356@gmail.com>
2025-07-04 09:51:31 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+//how to add the element in html and after that insert content in that and add styling 
+// let heading=document.createElement("h3");
+// heading.innerHTML="HI I M BLUE HEADING";
+// document.querySelector("body").append(heading);
+
+// heading.classList("blue"); //blue is class name of variable heading 
+
+// let greenbtn=document.querySelector(".artist-card");
+// greenbtn.classList.add("green-button");
+const play=function(){
+let mySound = new Audio('Jind_Mahi (1).mp3');
+mySound.play();
+console.log("button clicked");
+}
+
+
+
